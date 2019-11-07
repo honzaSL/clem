5d0227cebf8ffe7e8a8581dcc7527f02b7e93b00
honzaSL <54447127+honzaSL@users.noreply.github.com>
2019-11-07 08:30:47 +0100

    Create help.js

---
+++ help.js
@@ -0,0 +1,61 @@
+const Discord = require('discord.js');
+
+function help(message){
+const helpmsg = {
+	color: 0x0099ff,
+	title: 'Commadlist',
+	fields: [
+		/*{
+			name: 'Setprefix',
+			value: 'cahnge prefix',
+			
+		},*/
+		{
+			
+			name: 'Play',
+			value: 'Start music and add to queue, message must contain youtube song link',
+		},
+		{
+			name: 'Skip',
+			value: 'Skip current song',
+		},
+		{
+			name: 'Stop',
+			value: 'Stop music',
+			
+		},
+		{
+			name: 'Create_list',
+			value: 'Create your personal playlist',
+			
+		},
+		{
+			name: 'Listadd',
+			value: 'Add song in your playlist, must contain youtube song link',
+			
+		},
+		{
+			name: 'Startlist',
+			value: 'start your list, skip and stop have same function as before',
+			
+		},
+		{
+			name: 'Cat',
+			value: 'send random cat picture',
+			
+		},
+		{
+			name: 'Eidolons',
+			value: 'send time and day state on Plain of Eidolons',
+			
+		},
+		
+		
+	],
+
+	timestamp: new Date(),
+	
+};
+return message.channel.send({ embed: helpmsg });
+}
+module.exports.help = help; // export your functuion
